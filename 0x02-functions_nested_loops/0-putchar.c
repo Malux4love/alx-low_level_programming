6be80a505133aacc9fef8ca6051b52ab65a1faae
Malux4love <malux4love@gmail.com>
2022-09-14 05:46:15 -0700

    Task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+<<<<<<< HEAD
 /**
  * main - print putchar
  * Return: always 0
@@ -19,4 +20,46 @@ int main(void)
 
 	return (0);
 
+=======
+
+
+/**
+
+ * main - print putchar
+
+ * Return: always 0
+
+ */
+
+
+
+int main(void)
+
+{
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+
+
+	return (0);
+
+
+
+>>>>>>> Task 0
 }
