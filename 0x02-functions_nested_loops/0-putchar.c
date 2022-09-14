0c164620b39860e53684128ab5393b73acd6cbb3
Malux4love <111126045+Malux4love@users.noreply.github.com>
2022-09-14 13:56:38 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-<<<<<<< HEAD
 /**
  * main - print putchar
  * Return: always 0
@@ -20,46 +19,4 @@ int main(void)
 
 	return (0);
 
-=======
-
-
-/**
-
- * main - print putchar
-
- * Return: always 0
-
- */
-
-
-
-int main(void)
-
-{
-
-	_putchar('_');
-
-	_putchar('p');
-
-	_putchar('u');
-
-	_putchar('t');
-
-	_putchar('c');
-
-	_putchar('h');
-
-	_putchar('a');
-
-	_putchar('r');
-
-	_putchar('\n');
-
-
-
-	return (0);
-
-
-
->>>>>>> Task 0
 }
